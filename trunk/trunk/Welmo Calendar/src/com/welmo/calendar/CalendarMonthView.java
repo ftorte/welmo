50a434493445f05d3353166cacc92f681e56535f
25Apr1945 <25Apr1945@c0e49051-f347-0410-8db0-3d0250ffe241>
2008-03-08 23:31:04 +0000

    Version New

--- trunk/trunk/Welmo Calendar/src/com/welmo/calendar/CalendarMonthView.java
+++ trunk/trunk/Welmo Calendar/src/com/welmo/calendar/CalendarMonthView.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class CalendarMonthView extends Activity {
     /** Called when the activity is first created. */
+	// example of change and update in the repository
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
